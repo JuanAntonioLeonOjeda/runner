4c744cce39c362caf22a59e88abee56f3c343fe1
JuanAntonioLeonOjeda <jalojr@hotmail.com>
2023-07-03 19:38:22 +0100

    character selected

--- script.js
+++ script.js
@@ -2,10 +2,17 @@ import { characterScreen } from "./characterScreen.js"
 
 const board = document.getElementById('main')
 const startButton = document.getElementsByClassName('start-button')[0]
+let character = ''
 
 startButton.addEventListener('click', () => {
   removeChildren(board)
   loadCharacterScreen()
+  const options = document.getElementsByClassName('character-select')
+  for (let i = 0; i < options.length; i++) {
+    options[i].addEventListener('click', () => {
+      character = i
+    })
+  }
 })
 
 function removeChildren(element) {
