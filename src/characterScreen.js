d19a52e75c25658dbecb3f85afbb2d705ac5a5df
JuanAntonioLeonOjeda <jalojr@hotmail.com>
2024-10-06 12:41:26 +0100

    More style change

--- src/characterScreen.js
+++ src/characterScreen.js
@@ -13,7 +13,7 @@ const characterScreen = `
       </div>
      <div id="aux"></div> 
       <div class="text-container">
-        <span class="text">Cae más lento cuando mantienes pulsado</span>
+        <span class="text">Mantén pulsado para flotar al caer</span>
       </div>
     </div>
     <div class="character-select">
